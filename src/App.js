91afd01be21c374accea0033cccff4128a7c3541
esdrasli <42820970+esdrasli@users.noreply.github.com>
2020-09-15 15:56:49 -0300

    comitando

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/* eslint-disable*/
+
+import React from "react";
+import "./App.css";
+import KommunicateChat from "./chat";
+import Presentation from "./apres";
+
+function App() {
+  return (
+    <div>      
+      <KommunicateChat />
+      <Presentation />
+    </div>
+  );
+}
+
+export default App;
