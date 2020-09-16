7296b939a809ff7f9651daf38a7cffdb0e99d3f8
esdrasli <42820970+esdrasli@users.noreply.github.com>
2020-09-15 23:47:45 -0300

    apresentação

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@
 import React from "react";
 import "./App.css";
 import KommunicateChat from "./chat";
-//import Presentation from "./apres";
+import Presentation from "./apres";
 
 function App() {
   return (
     <div>      
       <KommunicateChat />
-      
+      <Presentation />
     </div>
   );
 }
